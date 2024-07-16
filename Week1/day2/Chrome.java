426a19ea7d367983dd27285b808c38c304176579
JrbBALAJI <balajiramamoorthy1984@gmail.com>
2024-07-16 20:49:51 +0530

    Add files via upload
    Week1.day2 Assigment completed data type and sysout exercise

---
+++ Week1/day2/Chrome.java
@@ -0,0 +1,20 @@
+package Week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear= 2008;
+		char shortcutKey ='c';
+		
+		System.out.println("Version:" +version);
+		System.out.println("Developer :"+developer);
+		System.out.println("IsBeta :"+isBeta);
+		System.out.println("ReleaseYear :"+releaseYear);
+		System.out.println("ShortcutKey :" +shortcutKey);
+	}
+
+}
